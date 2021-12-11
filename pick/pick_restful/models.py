42477ba5bbc039480d1379648d035158177a2c5d
pseong <pkseonguk@gmail.com>
2021-12-11 14:56:43 +0000

    Add DB User table to models.py

--- pick/pick_restful/models.py
+++ pick/pick_restful/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class SocialPlatform(models.Model):
+    platform = models.CharField(max_length=20, default=0)
+
+    class Meta:
+        db_table = "social_platform"
+
+class User(models.Model):
+    last_login      = models.DateTimeField('date published')
+    date_joined     = models.DateTimeField('date published', default=datetime.datetime.now)
+    date_birth      = models.DateField()
+    email           = models.CharField(max_length=254)
+    username        = models.CharField(max_length=254)
+    social          = models.ForeignKey(SocialPlatform, on_delete=models.CASCADE, max_length=20, blank=True, default=1)
+    social_login_id = models.CharField(max_length=50, blank=True)
