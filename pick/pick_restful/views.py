6b3527e48eed196877ab21675ac17e3fd698c61a
pseong <pkseonguk@gmail.com>
2021-12-11 16:16:06 +0000

    Add google login to views.py

--- pick/pick_restful/views.py
+++ pick/pick_restful/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import User
+from django.views import View
 
 # Create your views here.
 
 def index(request):
         return HttpResponse("백엔드서버입니다.")
+
+class GoogleLoginView(View): 
+        def get(self,request):
+                token = request.headers["Auth"]
+                url = 'https://oauth2.googleapis.com/tokeninfo?id_token='
+                response = requests.get(url+token)
+                user = response.json()
+
+                if User.objects.filter(social_login_id = user['sub']).exists():
+                        user_data = User.objects.get(social_login_id=user['sub'])
+                        encoded_jwt = jwt.encode({'id': user["sub"]}, wef_key, algorithm='HS256')
+
+                        return JsonResponse({
+                                'access_token'  : encoded_jwt.decode('UTF-8'),
+                                'user_name'     : user['name'],
+                                'user_id'       : user_data.id
+                        }, status = 200)
+                else:
+                        user_data = User(social_login_id = user['sub'],
+                                name = user['name'],
+                                social = SocialPlatform.objects.get(platform ="google"),
+                                email = user.get('email', None)
+                        )
+                        user_data.save()
+                        encoded_jwt = jwt.encode({'id': user_data.id}, wef_key, algorithm='HS256')
+
+                        return JsonResponse({
+                                'user_id'       : user_data.id,
+                                'user_name'     : user_data.username,
+                                'access_token'  : encoded_jwt.decode('UTF-8')
+                        }, status = 200)
