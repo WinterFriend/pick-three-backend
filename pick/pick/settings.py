fc6a9e307b43dd9e43ea099eb14c1efc349296f7
pseong <pkseonguk@gmail.com>
2021-12-12 08:08:58 +0000

    Remove SECRET_KEY for secrets.json that is hidden by .gitignore

--- pick/pick/settings.py
+++ pick/pick/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os, json
+from django.core.exceptions import ImproperlyConfigured
 import getpass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,15 +23,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7wcy4o=ak2bd&9stj&o(!=lb5a7d#yxc*x6ki54rarrlps*u9+'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "no {} variable in secrets.json".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['3.37.254.13']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'rest_framework',
@@ -80,7 +89,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': "/home/"+getpass.getuser()+"/back-end/cnf/mysql.cnf",
+            'read_default_file': get_secret("READ_DEFAULT_FILE"),
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
