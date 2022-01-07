70bb73759015de0354ceac34a3f8bcbad7358add
pseong <pkseonguk@gmail.com>
2022-01-08 02:29:21 +0900

    Fix service.py for beauty code

--- pick/pick_restful/services.py
+++ pick/pick_restful/services.py
@@ -1,11 +1,10 @@
 from typing import Tuple
-
 from django.db import transaction
 from django.core.management.utils import get_random_secret_key
-from utils import get_now
+from rest_framework_simplejwt.tokens import RefreshToken
 from pick_restful.models import User, SocialPlatform, UserGoal, Goal
 from django.utils import timezone
-from rest_framework_simplejwt.tokens import RefreshToken
+from utils import get_now
 
 def user_create_superuser(id, password=None, **extra_fields) -> User:
     extra_fields = {
@@ -15,18 +14,26 @@ def user_create_superuser(id, password=None, **extra_fields) -> User:
         'id': id
     }
 
-    user = user_create('0', social="google", password=password, **extra_fields)
+    user = user_create('0', social="google",
+                        password=password, **extra_fields)
 
     return user
 
-def user_create(sub, social, password=None, **extra_fields) -> User:
+def user_create(
+    sub: str, 
+    social: str, 
+    password: str = None, 
+    **extra_fields
+) -> User:
     extra_fields = {
         'is_staff': False,
         'is_superuser': False,
         **extra_fields
     }
 
-    user = User(sub=sub, social=SocialPlatform.objects.get(platform=social), **extra_fields)
+    user = User(sub=sub,
+                social=SocialPlatform.objects.get(platform=social), 
+                **extra_fields)
 
     if password:
         user.set_password(password)
@@ -53,9 +60,15 @@ def user_change_secret_key(*, user: User) -> User:
     return user
 
 @transaction.atomic
-def user_get_or_create(*, sub: str, social: str, **extra_data) -> Tuple[User, bool]:
-    user = User.objects.filter(sub=sub).first() # 이거 필터에서 소셜도 넣어야함 나중에 꼭!
-    
+def user_get_or_create(
+    *, 
+    sub: str, 
+    social: str, 
+    **extra_data
+) -> Tuple[User, bool]:
+
+    # 나중에 필터에 소셜도 필요할 수 있음
+    user = User.objects.filter(sub=sub).first()
     if user:
         user.last_login = timezone.localtime()
         user.save()
@@ -65,22 +78,23 @@ def user_get_or_create(*, sub: str, social: str, **extra_data) -> Tuple[User, bo
 
 def jwt_login(user: User):
     refresh = RefreshToken.for_user(user)
-
-    print(  'refresh_token : ',     str(refresh))
-    print(  'access_token : ',      str(refresh.access_token))
-
     return {
-        'refresh':      str(refresh),
-        'access':       str(refresh.access_token),
+        'refresh':  str(refresh),
+        'access':   str(refresh.access_token),
     }
 
 @transaction.atomic
-def user_goal_detail_set(date: str, user_id: str, userGoalList: list, updateColumn: list):
+def user_goal_detail_set(
+    date: str, 
+    user_id: str, 
+    userGoalList: list, 
+    updateColumn: list
+) -> None:
     user_goal = UserGoal.objects.filter(select_date=date, user_id=user_id)
     user_goal.update(active=0)
     for obj in userGoalList:
         defaults = {column : obj[column] for column in updateColumn}
         defaults['active'] = 1
         print(defaults)
-        UserGoal.objects.update_or_create(select_date=date, user=User.objects.get(id=user_id), goal=Goal.objects.get(id=int(obj['goalId'])), 
-            defaults=defaults)
+        UserGoal.objects.update_or_create(select_date=date, user=User.objects.get(id=user_id), 
+            goal=Goal.objects.get(id=int(obj['goalId'])), defaults=defaults)
